b30f985dbf4116c41359292b2ea2447d6da91de4
shuzhi-cloud <403897808@qq.com>
2024-05-29 21:03:13 +0800

    循环和技巧

---
+++ JAVA/LearnJava/Learn12/Learn12.java
@@ -0,0 +1,15 @@
+package NewLearn.Learn12;
+//快捷键Ctrl+insert创建,Ctrl+shift+F12窗口最大化
+//alt+1打开窗口，连敲两次shift查找某个类,alt+左右键切换选项卡
+public class Learn12 {
+
+    public static void main(String[] args) {//psvm自动生成main
+        System.out.println();//,sout自动生成输出语句
+        double pai = 3.14;//使用.var快捷键
+        //Ctrl+Y删除一行，Ctrl+D复制一行，Ctrl+Z撤销
+        for (int i = 0; i < 12; i++) {
+            //fori会直接建立for循环
+            //Ctrl+F12在类中查找方法
+        }
+    }
+}
