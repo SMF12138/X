c391c63449b8247c7b5e17725584d838ba702857
shuzhi-cloud <403897808@qq.com>
2024-06-01 20:19:41 +0800

    关于this

---
+++ JAVA/LearnJava/Learn14/Learn14.java
@@ -0,0 +1,9 @@
+package NewLearn.Learn14;
+
+public class Learn14 {
+    public static void main(String[] args) {
+        Student ZXR = new Student();
+        ZXR.name = "ZXR";
+        ZXR.study();
+    }
+}
---
+++ JAVA/LearnJava/Learn14/Student.java
@@ -0,0 +1,8 @@
+package NewLearn.Learn14;
+
+public class Student {
+    String name;
+    public void study(){
+        System.out.println(this.name);//这里this代表了调用这个函数的实例变量,但this可省
+    }
+}
