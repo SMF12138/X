3cda90df58335c8b77170a4e23c7444ed7325fa7
shuzhi-cloud <403897808@qq.com>
2024-06-13 20:39:55 +0800

    关于重写

---
+++ JAVA/LearnJava/Learn25/Animal.java
@@ -0,0 +1,10 @@
+package NewLearn.Learn25;
+
+public class Animal {
+    public void eat(){
+        System.out.println("动物在吃");
+    }
+    public void run(){
+        System.out.println("动物在跑");
+    }
+}
---
+++ JAVA/LearnJava/Learn25/Bird.java
@@ -0,0 +1,8 @@
+package NewLearn.Learn25;
+
+public class Bird extends Animal{
+    @Override//这个注解可以在编译阶段判断是否重写，如果不是会报错
+    public void eat(){
+        System.out.println("鸟在吃");
+    }
+}
