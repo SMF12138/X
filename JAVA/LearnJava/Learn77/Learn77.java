1ae135185d667b2cce712f29d5f29fb9d032e8ef
shuzhi-cloud <403897808@qq.com>
2024-08-09 20:32:15 +0800

    Math类

---
+++ JAVA/LearnJava/Learn77/Learn77.java
@@ -0,0 +1,19 @@
+package NewLearn.Learn77;
+
+public class Learn77 {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);//圆周率
+        int a = 100;
+        int b = 396;
+        System.out.println(Math.abs(a - b));//绝对值
+        System.out.println(Math.ceil(1.3));//向上取整
+        System.out.println(Math.floor(1.9));//向下取整
+        System.out.println(Math.max(1, 2));
+        System.out.println(Math.min(1, 2));
+        System.out.println(Math.round(3.5));//四舍五入
+        System.out.println(Math.sqrt(10));//开根号
+        System.out.println(Math.pow(2, 2));//次方
+        System.out.println(Math.random());//生成[0,1)的随机数
+
+    }
+}
