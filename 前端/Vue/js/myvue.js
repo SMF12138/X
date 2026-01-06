36e4dcbf7d1dc85e680e4a185eee6068cd718e7d
shuzhi-cloud <403897808@qq.com>
2026-01-06 21:08:05 +0800

    近期的Vue实践

--- 前端/Vue/js/myvue.js
+++ 前端/Vue/js/myvue.js
@@ -28,5 +28,10 @@ class Vue {
 				});
 			}
 		});
+		// 获取所有的方法名
+		Object.keys(options.methods).forEach((methodName, index) => {
+			console.log(methodName);
+			this[methodName] = options.methods[methodName];
+		});
 	}
 }
